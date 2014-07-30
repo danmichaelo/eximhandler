4d96e36d581dc8a3587df6f58545cbaf56de8808
Dan Michael O. Heggø <danmichaelo@gmail.com>
2014-07-30 03:44:15 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding=utf8
+
+from distutils.core import setup
+
+setup(name='EximHandler',
+      version='1.0',
+      description='A logging handler class which sends an email using exim',
+      author='Dan Michael O. Heggø',
+      author_email='danmichaelo@gmail.com',
+      url='https://github.com/danmichaelo/eximhandler',
+      packages=['eximhandler'],
+      )
